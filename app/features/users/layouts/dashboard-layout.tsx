0c9214a84611ca91e7151d78aa335a37012c5a70
Paul Jang <happyness980817@gmail.com>
2025-12-22 14:09:51 +0900

    Dashboard Page

---
+++ app/features/users/layouts/dashboard-layout.tsx
@@ -0,0 +1,69 @@
+import { HomeIcon, LightbulbIcon, RocketIcon } from "lucide-react";
+import { Link, Outlet, useLocation } from "react-router";
+import {
+  SidebarProvider,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarGroupLabel,
+  SidebarMenuItem,
+} from "~/common/components/ui/sidebar";
+
+export default function DashboardLayout() {
+  const location = useLocation();
+  return (
+    <SidebarProvider className="flex min-h-full">
+      <Sidebar className="pt-16" variant="floating">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  asChild
+                  isActive={location.pathname === "/my/dashboard"}
+                >
+                  <Link to="/my/dashboard">
+                    <HomeIcon className="size-4" />
+                    <span>Home</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  asChild
+                  isActive={location.pathname === "/my/dashboard/ideas"}
+                >
+                  <Link to="/my/dashboard/ideas">
+                    <LightbulbIcon className="size-4" />
+                    <span>Ideas</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel>Products Analytics</SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  asChild
+                  isActive={location.pathname === "/my/dashboard/products/1"}
+                >
+                  <Link to="/my/dashboard/products/1">
+                    <RocketIcon className="size-4" />
+                    <span>Product 1</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+      <div className="w-full h-full">
+        <Outlet />
+      </div>
+    </SidebarProvider>
+  );
+}
--- app/features/users/pages/dashboard-ideas-page.tsx
+++ app/features/users/pages/dashboard-ideas-page.tsx
@@ -1,3 +1,4 @@
+import { IdeaCard } from "~/features/ideas/components/idea-card";
 import type { Route } from "./+types/dashboard-ideas-page";
 
 export const meta: Route.MetaFunction = () => {
@@ -5,6 +6,22 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function DashboardIdeasPage({}: Route.ComponentProps) {
-  return <div></div>;
+  return (
+    <div className="space-y-5">
+      <h1 className="text-2xl font-semibold mb-6">Your Claimed Ideas</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <IdeaCard
+            key={index}
+            id={`ideaId-${index}`}
+            title="AI-driven marketplace that matches underutilized industrial equipment with startups, handles financing, and optimizes logistics to reduce downtime."
+            viewCount={100}
+            timestamp="12 hours ago"
+            likesCount={100}
+            claimed={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-
