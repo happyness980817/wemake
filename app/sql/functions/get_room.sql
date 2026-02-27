3672b858154c8a9d328ea02a129c1d8e37a1531c
Paul Jang <happyness980817@gmail.com>
2026-02-27 10:57:20 +0900

    10.8 Function Search Path

--- app/sql/functions/get_room.sql
+++ app/sql/functions/get_room.sql
@@ -2,12 +2,12 @@ CREATE OR REPLACE FUNCTION public.get_room(from_user_id uuid, to_user_id uuid)
 RETURNS TABLE (message_room_id bigint)
 LANGUAGE plpgsql
 SECURITY DEFINER
-SET search_path = ''
+SET search_path = '' -- 'profiles' 대신 'public.profiles' 처럼 모든 객체를 명시적으로 지정해야 함, 보안 취약점 예방, supabase 권장 설정
 AS $$
 BEGIN
     RETURN QUERY SELECT m1.message_room_id
-    FROM message_room_members m1
-    INNER JOIN message_room_members m2
+    FROM public.message_room_members m1
+    INNER JOIN public.message_room_members m2
     ON m1.message_room_id = m2.message_room_id
     WHERE m1.profile_id = from_user_id
     AND m2.profile_id = to_user_id;
